7116a1f0d9bf7787408c2028811139a3e3d25e95
ian <ianxm@jhu.edu>
2012-01-18 23:02:51 -0500

    added convenience date routines

--- src/utils/TestUtils.hx
+++ src/utils/TestUtils.hx
@@ -12,6 +12,34 @@ class TestUtils extends haxe.unit.TestCase
         assertEquals("2012-02-29", Utils.dayStr(new Date(2012, 01, 29, 0, 0, 0)));
     }
 
+    public function testDay()
+    {
+        assertEquals("2010-01-01 00:00:00", Utils.day(new Date(2010, 00, 01, 0, 0, 0)).toString());
+        assertEquals("2012-02-29 00:00:00", Utils.day(new Date(2012, 01, 29, 0, 0, 0)).toString());
+        assertEquals("2011-03-01 00:00:00", Utils.day(new Date(2011, 01, 29, 0, 0, 0)).toString());
+    }
+
+    public function testDayToStr()
+    {
+        assertEquals("2010-01-01", Utils.dayToStr(new Date(2010, 00, 01, 1, 0, 0)).toString());
+        assertEquals("2012-02-29", Utils.dayToStr(new Date(2012, 01, 29, 0, 2, 0)).toString());
+        assertEquals("2011-03-01", Utils.dayToStr(new Date(2011, 01, 29, 0, 0, 3)).toString());
+    }
+
+    public function testDayShift()
+    {
+        assertEquals("2010-01-02 00:00:00", Utils.dayShift(new Date(2010, 00, 01, 0, 0, 0), 1).toString());
+        assertEquals("2009-12-31 00:00:00", Utils.dayShift(new Date(2010, 00, 01, 0, 0, 0), -1).toString());
+    }
+
+    public function testDayDelta()
+    {
+        assertEquals(2, Utils.dayDelta(new Date(2010, 00, 01, 0, 0, 0), new Date(2010, 00, 03, 0, 0, 0)));
+        assertEquals(-2, Utils.dayDelta(new Date(2010, 00, 03, 0, 0, 0), new Date(2010, 00, 01, 0, 0, 0)));
+        assertEquals(-2, Utils.dayDelta(new Date(2010, 00, 01, 0, 0, 0), new Date(2009, 11, 30, 0, 0, 0)));
+        assertEquals(3, Utils.dayDelta(new Date(2009, 11, 29, 0, 0, 0), new Date(2010, 00, 01, 0, 0, 0)));
+    }
+
     public function testZeroFill()
     {
         assertEquals("01", Utils.zeroFill(1, 2));
--- src/utils/Utils.hx
+++ src/utils/Utils.hx
@@ -12,6 +12,21 @@ class Utils
         return new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0);
     }
 
+    inline public static function dayToStr( date :Date ) :String
+    {
+        return date.toString().substr(0, 10);
+    }
+
+    inline public static function dayShift( date :Date, days :Int )
+    {
+        return DateTools.delta(date, days*1000*60*60*24);
+    }
+
+    inline public static function dayDelta( date1 :Date, date2 :Date ) :Int
+    {
+        return Std.int((date2.getTime()-date1.getTime())/(1000*60*60*24));
+    }
+
     public static function zeroFill( num :Int, width :Int ) :String
     {
         var str = Std.string(num);
